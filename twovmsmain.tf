a00898f695b3191d95e04bb35e1af6f8500bd33d
Nicholas Gulrajani <nicholas.gulrajani@cdw.com>
2023-06-28 13:07:16 -0400

    WIP

--- twovmsmain.tf
+++ twovmsmain.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.0"
+    }
+  }
+}
 resource "random_pet" "rg_name" {
   prefix = var.resource_group_name_prefix
 }
